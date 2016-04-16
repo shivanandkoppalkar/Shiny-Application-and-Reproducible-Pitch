d502e97d9a310929d51b71f5034e3a5ec48e47b5
shivanandkoppalkar <shivanand.koppalkar@gmail.com>
2016-04-16 18:43:44 +0100

    "R" Scripts for UI & Server
    "R" Scripts for UI & Server

--- server.R
+++ server.R
@@ -1,49 +1,161 @@
+#      Coursera Developing Data Products course project
+#
+#      This is the Server-side script for the shiny application built as part of the
+#      project. This script filters the list of cities based on the user input and
+#      also builds the output map and tables.
+#      
+#      Date            Developer           Comments
+#      2016 Apr 16     Shamik Mitra        Initial Version
+
 library(shiny)
-library(UsingR)
-data(camp)
+library(leaflet)
+
+
+# Read the data for the list of cities and countries. This is executed once at the beginning of the server
+DataCities <- read.csv("data/cities.csv",stringsAsFactors=FALSE)
+DataCountries <- read.csv("data/countries.csv",stringsAsFactors=FALSE)
+DataCountries <- DataCountries[order(DataCountries$Country),]
+
+
+
+# This function takes the user input and creates a subset of cities to be displayed
+# with the calculated Quality of Life index.
+FilterData <- function(input) {
+    
+    # Create a subset of the data based on whether the user has selected a region and country
+    if(input$ShowRegion != "World") {
+        if(input$ShowCountry != "All countries") {
+            ChartCities <- DataCities[DataCities$Country == input$ShowCountry,]
+        } else {
+            ChartCities <- DataCities[DataCities$Region == input$ShowRegion,]
+        }
+    } else {
+        ChartCities <- DataCities
+    }
+    
+    # Calculate the Quality of Life index based on the user input weights
+    ChartCities$QualityOfLife <- round(100 + (input$PurchasingPowerWeight * ChartCities$PurchasingPower / 100) 
+                                           + (input$SafetyWeight * ChartCities$Safety / 100) 
+                                           + (input$HealthCareWeight * ChartCities$HealthCare / 100) 
+                                           - (input$CostOfLivingWeight * ChartCities$CostOfLiving / 100) 
+                                           - (input$PropertyToIncomeWeight * ChartCities$PropertyToIncome / 100) 
+                                           - (input$TrafficCommuteWeight * ChartCities$TrafficCommute / 100) 
+                                           - (input$PollutionWeight * ChartCities$Pollution / 100) 
+                                           + (input$ClimateWeight * ChartCities$Climate / 100),1)
+    
+    # Create the description field, which will be used as popup description for the cities
+    ChartCities$Description <- paste(paste(ChartCities$City, ChartCities$Country, sep = ", "),paste("Population:", round(ChartCities$Population/1000000,1), "million", sep = " "),paste("Quality of Life", ChartCities$QualityOfLife, sep = ": "), sep = "  |  ")
+    
+    # Sort the cities with the highest Quality of Life index first
+    ChartCities <- ChartCities[order(-ChartCities$QualityOfLife),]
+    
+    # Return the filtered list of cities
+    ChartCities
+}
+
+# This function takes in the list of filtered cities created by the function above and returns a map
+FormOutputMap <- function(ChartCities) {
+
+    # Create a gradient color pallette based on the value of Quality of Life index
+    pal <- colorNumeric(
+        palette = c("firebrick1", "orange", "yellow", "yellowgreen", "limegreen"),
+        domain = ChartCities$QualityOfLife
+    )
+
+                # Create a leaflet map with the list of cities    
+    OutputMap <- leaflet(ChartCities) %>%
+        
+                # Add the tiles from Thunderforest
+                 addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png", 
+                          attribution = '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>') %>%
+        
+                # Add the Circle markers for the cities
+                 addCircleMarkers(lng=~Longitude,
+                                   lat=~Latitude,
+                                   radius=7,
+                                   color = ~pal(QualityOfLife),
+                                   fillOpacity = 0.8,
+                                   weight=1,
+                                   popup=~Description,
+                                   stroke=TRUE) %>%
+                # Add a legend
+                 addLegend("bottomright", pal = pal, values = ~QualityOfLife,
+                           title = "Quality of Life", opacity = 0.8)
+
+    # Return the map
+    OutputMap
+}
+
+
+
+# This function takes in the filtered list of cities returned by the function above and returns a
+# table with the best and worst 3 cities. If total  number of cities filtered by the user is less
+# than 6 then it will return only one list of cities.
+FormOutputCities <- function(ChartCities) {
+    
+    # Create a copy of the list of cities with fewer columns
+    ChartCities <- ChartCities[,c("City","Country","Population","QualityOfLife")]
+    
+    # If there are multiple countries in the list of cities, then append that to the city
+    if(length(unique(ChartCities$Country))>1) {
+        ChartCities$City <- paste(ChartCities$City, ChartCities$Country, sep=", ")
+    }
+    
+    # Drop the country column
+    ChartCities <- ChartCities[,-2]
+    
+    # Conver the population column into millions
+    ChartCities$Population <- paste(round(ChartCities$Population/1000000,1),"million",sep=" ")
+    
+    if(nrow(ChartCities)<6) {
+        
+        # If there are less than 6 cities, then the output is just the filtered list of cities
+        OutputTable <- ChartCities[order(-ChartCities$QualityOfLife),]
+        
+    } else {
+        
+        # If there are more than 6 cities, then sort by the index descending and take the top 3 cities
+        ChartCities <- ChartCities[order(-ChartCities$QualityOfLife),]
+        Top3 <- ChartCities[1:3,]
+        
+        # Then sort by the index ascending and take the bottom 3 cities
+        ChartCities <- ChartCities[order(ChartCities$QualityOfLife),]
+        Bottom3 <- ChartCities[1:3,]
+        
+        # The output is the list of top and bottom 3 cities joined together
+        OutputTable <- cbind(Top3, Bottom3)
+        
+        # Update the list of names - the City column is renamed to Best and Worst cities
+        names(OutputTable) <- c("Best Cities", "Population", "QualityOfLife", "Worst Cities", "Population", "QualityOfLife")
+    }
+    
+    # Return the output table
+    OutputTable
+}
+
+
+
+# This function returns a subset of countries for the input region. This function is
+# called by the script below that is executed when the user selects a region
+CountrySubset <- function(InputRegion){
+    c("All countries",DataCountries[DataCountries$Region==InputRegion,]$Country)
+}
+
+
+
+# This is the main server function that calls the functions above to form the output
 shinyServer(
-  function(input, output) {
-    camp.spline1 <- reactive({smooth.spline(x = camp, spar = input$spar1)})
-    camp.spline2 <- reactive({smooth.spline(x = camp - camp.spline1()$y, spar = input$spar2)})
-    output$splinePlot1 <- renderPlot({
-      par(plt = c(0.2,0.90,0.1,0.95))
-      plot.default(x = camp, main = 'Annual Tree Ring Increment',
-                   xlim = input$xrange,
-                   type = 'p', pch = 1)
-      lines(camp.spline1(), col = 2, lwd = 3)
-    })
-    output$splinePlot2 <- renderPlot({
-      par(plt = c(0.2,0.90,0.2,0.95))
-      plot.default(x = camp - camp.spline1()$y,
-                   xlab = 'Year',
-                   xlim = input$xrange,
-                   type = 'p', pch = 1,
-                   ylab = "Detrended Data")
-      lines(camp.spline2(), col = 4, lwd = 3)
-    })
-    output$residuals1 <- renderPlot({
-      par(plt = c(0.2,0.90,0.2,0.95))
-      hist(x = camp - camp.spline1()$y, 
-           main = "Residuals after first smoothing",
-           xlab = "Residual tree ring increment (mm)")
-    })
-    output$residSum1 <- renderText({
-      paste("Sum of first spline residuals = ",
-      format(sum(abs(camp - camp.spline1()$y)), digits = 2))
-      })
-    output$residuals2 <- renderPlot({
-      par(plt = c(0.2,0.90,0.2,0.95))
-      hist(x = camp - camp.spline1()$y - camp.spline2()$y, 
-           main = "Residuals after second smoothing",
-           xlab = "Residual tree ring increment (mm)")
-    })
-    output$residSum2 <- renderText({
-      paste("Second spline residuals = ",
-          format(sum(abs(camp - camp.spline1()$y - camp.spline2()$y)), digits = 2))
-    })
-    output$residSum3 <- renderText({
-      paste("Some of both spline residuals = ",
-            format(sum(sum(abs(camp - camp.spline1()$y)),sum(abs(camp - camp.spline1()$y - camp.spline2()$y))), digits = 2))
-    })
-  }
+    function(input, output, session) {
+        
+        # This function observes if the user has changed the region field, and if yes, returns a subset of countries
+        observe({
+            if(input$ShowRegion != "World") {
+                updateSelectInput(session, "ShowCountry", choices = CountrySubset(input$ShowRegion), selected = "All countries")
+            }
+        })
+        
+        # Call the functions to form the map and table outputs based on the user input
+        output$QoLMap <- renderLeaflet(FormOutputMap(FilterData(input)))
+        output$QoLTable <- renderTable(FormOutputCities(FilterData(input)))
+    }
 )
--- ui.R
+++ ui.R
@@ -1,28 +1,303 @@
+#      Coursera Developing Data Products course project
+#
+#      This is the UI script for the shiny application built as part of the course 
+#      project. The UI uses a fluidPage layout with a fixed panel on the left that 
+#      has the sliders for changing the weights for calculating the Quality of Life 
+#      index. It has a map display in the middle section which is rendered using 
+#      leaflet. The right had side has a floating panel which has the controls to 
+#      filter the list of cities. On the bottom of the screen, there is a table with
+#      the list of best and worst cities.
+#      
+#      Date            Developer           Comments
+#      2016 Apr 16     Shamik Mitra        Initial Version
+
+library(shiny)
+library(leaflet)
+
+# Load the list of regions, which will be used to populate the drop down list for regions
+DataRegions <- read.csv("data/regions.csv",stringsAsFactors=FALSE)
+
+# Set the default region
+ShowRegion <- "World"
+
 shinyUI(fluidPage(
-  titlePanel("Investigating time scales of variability in tree ring data"),
-  h4('This Shiny App allows you to detrend a long term time series of tree ring data with a smoothing spline and visualise the residuals. A second smoothing spline is fitted to the residuals. Set the time frame you wish to visualise and the the smoothing spline parameters with the sliders. Have the temporal scales of tree ring increment variability changed in the the past 5000 years?'),
-  fluidRow(
-    column(3,
-           wellPanel(
-             h3("Plot Parameters"),
-             h4("Set Timeframe"),
-             sliderInput('xrange', 'Enter Range of Years', value = c(-3435,1969), min = -3435, max = 1969, step = 1),
-             h4("Set Spline Smoothing Parameters"),
-             sliderInput('spar1', 'Red Spline - raw data', value = 0.75, min = -1, max = 1.5, step = 0.01),
-             sliderInput('spar2', 'Blue Spline - detrended data', value = 0.75, min = -1, max = 1.5, step = 0.01)
-  ),
-  wellPanel(
-    h3("Spline Fit Statistics"),
-    textOutput('residSum1'),
-    textOutput('residSum2'),
-    textOutput('residSum3')
-  )
-  ),
-  column(9,
-      plotOutput('splinePlot1', width = '100%', height = "150px"),
-      plotOutput('residuals1', width = '100%', height = "150px"),
-      plotOutput('splinePlot2', width = '100%', height = "150px"),
-      plotOutput('residuals2', width = '100%', height = "150px")
+    title="Quality of Living",
+    
+    # Set the style
+    tags$head(
+        tags$style(HTML("
+                        body {
+                            background-color: #ADDEFF;
+                        }
+
+                        h2 {
+                            font-family: 'Verdana';
+                            font-weight: bold;
+                            font-size: large;
+                            color: black;
+                        }
+
+                        h4 {
+                            font-family: 'Trebuchet MS';
+                            font-weight: normal;
+                            font-size: xsmall;
+                            color: black;
+                        }
+                        
+                        table {
+                            font-family: 'Arial';
+                            font-weight: bold;
+                            font-size: small;
+                            color: black;
+                        }
+
+                        td {
+                            padding: 0px 0px 0px 10px;
+                        }
+
+                        #HeaderPanel {
+                            background-color: transparent;
+                            border-style: hidden;
+                            font-family: 'Trebuchet MS';
+                        }
+                        
+                        #OutputPanel {
+                            background-color: #ADDEFF;
+                            border-style: hidden;
+                            padding: 5px;
+                            font-family: 'Verdana';
+                            font-weight: normal;
+                            font-size: small;
+                            color: black;
+                            opacity: 0.4;
+                        }
+
+                        #OutputPanel:hover {
+                            opacity: 1;
+                            box-shadow: 2px 2px 5px #888888;
+                        }
+                        
+                        #HelpPanel {
+                            background-color: white;
+                            border-style: hidden;
+                            padding: 5px;
+                            font-family: 'Verdana';
+                            font-weight: normal;
+                            font-size: small;
+                            color: black;
+                            opacity: 0.4;
+                        }
+                        
+                        #HelpPanel:hover {
+                            opacity: 1;
+                            box-shadow: 2px 2px 5px #888888;
+                        }
+                                                
+                        #TopCitiesPanel {
+                            background-color: white;
+                            border-style: hidden;
+                            padding: 5px;
+                            font-family: 'Verdana';
+                            font-weight: normal;
+                            font-size: small;
+                            color: black;
+                            opacity: 0.4;
+                        }
+                        
+                        #TopCitiesPanel:hover {
+                            opacity: 1;
+                            box-shadow: 2px 2px 5px #888888;
+                        }
+                        
+                        #QoLTable {
+                            font-family: 'Arial';
+                            font-weight: normal;
+                            font-size: small;
+                            color: black;
+                            border: '0';
+                        }
+
+                        "))
+        ),
+    
+    fluidRow(
+        
+        # The first column has the panel for the slider inputs for the weights
+        column(width = 2,
+               h2("Select weights"),
+               tags$table(
+                   tags$tr(
+                       tags$td("Purchasing Power", 
+                               align="right", 
+                               valign="top", 
+                               width="30%"),
+                       tags$td(sliderInput(inputId="PurchasingPowerWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 40, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Cost of Living",
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="CostOfLivingWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 20, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Property to Income", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="PropertyToIncomeWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 100, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Safety", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="SafetyWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 50, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Health Care", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="HealthCareWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 40, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Traffic Commute", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="TrafficCommuteWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 50, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Pollution", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="PollutionWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 67, 
+                                           width = "100%"))
+                   ),
+                   tags$tr(
+                       tags$td("Climate", 
+                               align="right", 
+                               valign="top"),
+                       tags$td(sliderInput(inputId="ClimateWeight", 
+                                           label=NULL,
+                                           min = 0, 
+                                           max = 100, 
+                                           value = 50, 
+                                           width = "100%"))
+                   )
+               )
+        ),
+        
+        # The second column acts as the main section containing the leaflet output
+        column(width = 10,
+               leafletOutput('QoLMap', 
+                             height=600)
+        ),
+        
+        # A fixed panel is created on top of the map containing the title of the website
+        absolutePanel(id = "HeaderPanel", 
+                      class = "panel panel-default", 
+                      fixed = TRUE,
+                      draggable = FALSE, 
+                      top = "0%", 
+                      left = "22%", 
+                      right = "auto", 
+                      bottom = "auto",
+                      width = 800, 
+                      height = 60,
+                      
+                      h1("Quality of Life in major cities"),
+                      h4("By Shamik Mitra")
+                      
+        ),
+        
+        # A fixed panel is created on top-right with the help link
+        absolutePanel(id = "HelpPanel", 
+                      class = "panel panel-default", 
+                      fixed = TRUE,
+                      draggable = FALSE, 
+                      top = "0%", 
+                      left = "auto", 
+                      right = "1%", 
+                      bottom = "auto",
+                      width = 150, 
+                      height = "auto",
+                      
+                      a("Help documentation", href="https://shamikmitra.shinyapps.io/Help/", target="_blank")
+                      
+        ),
+        
+        # An additional floating panel is created on top of the map that contains the drop
+        # down lists for filtering the cities by region and country
+        absolutePanel(id = "OutputPanel", 
+                      class = "panel panel-default", 
+                      fixed = TRUE,
+                      draggable = TRUE, 
+                      top = "10%", 
+                      left = "auto", 
+                      right = "1%", 
+                      bottom = "auto",
+                      width = 150, 
+                      height = "auto",
+                      
+                      h2("Filter cities"),
+                      
+                      selectInput(inputId="ShowRegion", 
+                                  label="Region", 
+                                  choices = DataRegions$Region, 
+                                  selected = "World"),
+                      conditionalPanel(condition="input.ShowRegion != 'World'",
+                                       selectInput(inputId="ShowCountry", 
+                                                   label="Country", 
+                                                   choices = c("All countries"), 
+                                                   selected = "All countries"))
+        ),
+
+        # Lastly, a floating panel is created at the bottom of the map with the 
+        # list of best and worst cities
+        absolutePanel(id = "TopCitiesPanel", 
+                      class = "panel panel-default", 
+                      fixed = TRUE,
+                      draggable = TRUE, 
+                      top = "auto", 
+                      left = "22%", 
+                      right = "auto", 
+                      bottom = "5%",
+                      width = "auto", 
+                      height = "auto",
+
+                      tableOutput("QoLTable")
+
+        )
     )
-  )
 ))
